6386317d886c0b946c56daa60b084dc2b1ef8bb8
alvarogmsg <alvarogmsg99@gmail.com>
2023-08-17 17:37:43 -0600

    Initial Commit

---
+++ ProyectoFinal_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoFinal_iOS
+//
+//  Created by Álvaro Gómez Segovia on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
